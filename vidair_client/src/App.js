71cd2d6b23f2cf99e1f925b27a3b505071530f30
Yahya Bouhsine <81886762+YahyaBh@users.noreply.github.com>
2023-01-06 19:11:20 +0100

    make sidebar

--- vidair_client/src/App.js
+++ vidair_client/src/App.js
@@ -1,5 +1,5 @@
 import React, { Fragment } from 'react'
-import { Routes, Route } from 'react-router-dom/BrowserRouter';
+import { Routes, Route } from 'react-router-dom';
 import { Box } from '@mui/material';
 
 
--- vidair_client/src/Components/index.js
+++ vidair_client/src/Components/index.js
@@ -1,5 +1,7 @@
-export {default as Navbar} from './components/navbar/Navbar';
-export {default as VideoDetail} from './components/video-detail/VideoDetail';
-export {default as ChannelDetail} from './components/channel-detail/ChannelDetail';
-export {default as SearchFeed} from './components/search-feed/SearchFeed';
-export {default as Feed} from './components/feed/Feed';
+export { default as Navbar } from './Navbar/Navbar';
+export { default as VideoDetail } from './VideoDetail/VideoDetail';
+export { default as ChannelDetail } from './ChannelDetail/ChannelDetail';
+export { default as SearchFeed } from './SearchFeed/SearchFeed';
+export { default as SearchBar } from './SearchBar/SearchBar';
+export { default as Feed } from './Feed/Feed';
+export { default as SideBar } from './SideBar/SideBar';
